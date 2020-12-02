605f5a2da59497c52d39242c48f1c5a80a2d1870
Lavong Soysavanh <lavong.soysavanh@gmail.com>
2020-12-02 23:35:15 +0100

    day2 part2

--- src/main/kotlin/day2/day2.kt
+++ src/main/kotlin/day2/day2.kt
@@ -20,12 +20,29 @@ Each line gives the password policy and then the password. The password policy i
 In the above example, 2 passwords are valid. The middle password, cdefg, is not; it contains no instances of b, but needs at least 1. The first and third passwords are valid: they contain one a or nine c, both within the limits of their respective policies.
 
 How many passwords are valid according to their policies?
+
+--- Part Two ---
+
+While it appears you validated the passwords correctly, they don't seem to be what the Official Toboggan Corporate Authentication System is expecting.
+
+The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
+
+Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+
+Given the same example list from above:
+
+    1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+    1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+    2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+
+How many passwords are valid according to the new interpretation of the policies?
  */
 
 package day2
 
 data class PasswordEntry(val min: Int, val max: Int, val char: Char, val password: String) {
-    fun isValid() = password.count { it == char } in min..max
+    fun isValidPart1() = password.count { it == char } in min..max
+    fun isValidPart2() = listOf(password[min - 1], password[max - 1]).count { it == char } == 1
 }
 
 fun main() {
@@ -47,7 +64,9 @@ fun main() {
         }
         .also { println("password entries: ${it.size}") }
 
-    val validPasswordCount = passwordEntries.map { it.isValid() }.filter { it }.count()
+    passwordEntries.map { it.isValidPart1() }.filter { it }.count()
+        .also { println("valid passwords part1: $it") }
 
-    println("valid passwords: $validPasswordCount")
+    passwordEntries.map { it.isValidPart2() }.filter { it }.count()
+        .also { println("valid passwords part2: $it") }
 }
