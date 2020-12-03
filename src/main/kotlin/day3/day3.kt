6afed0fb13d76c454a431ef71389f1440db44263
Lavong Soysavanh <lavong.soysavanh@gmail.com>
2020-12-03 18:13:50 +0100

    day3 part1

---
+++ src/main/kotlin/day3/day3.kt
@@ -0,0 +1,102 @@
+/*
+--- Day 3: Toboggan Trajectory ---
+
+With the toboggan login problems resolved, you set off toward the airport. While travel by toboggan might be easy, it's certainly not safe: there's very minimal steering and the area is covered in trees. You'll need to see which angles will take you near the fewest trees.
+
+Due to the local geology, trees in this area only grow on exact integer coordinates in a grid. You make a map (your puzzle input) of the open squares (.) and trees (#) you can see. For example:
+
+..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#
+
+These aren't the only trees, though; due to something you read about once involving arboreal genetics and biome stability, the same pattern repeats to the right many times:
+
+..##.........##.........##.........##.........##.........##.......  --->
+#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+.#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+.#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....  --->
+.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+.#........#.#........#.#........#.#........#.#........#.#........#
+#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
+#...##....##...##....##...##....##...##....##...##....##...##....#
+.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+
+You start on the open square (.) in the top-left corner and need to reach the bottom (below the bottom-most row on your map).
+
+The toboggan can only follow a few specific slopes (you opted for a cheaper model that prefers rational numbers); start by counting all the trees you would encounter for the slope right 3, down 1:
+
+From your starting position at the top-left, check the position that is right 3 and down 1. Then, check the position that is right 3 and down 1 from there, and so on until you go past the bottom of the map.
+
+The locations you'd check in the above example are marked here with O where there was an open square and X where there was a tree:
+
+..##.........##.........##.........##.........##.........##.......  --->
+#..O#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+.#....X..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+..#.#...#O#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+.#...##..#..X...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+..#.##.......#.X#.......#.##.......#.##.......#.##.......#.##.....  --->
+.#.#.#....#.#.#.#.O..#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+.#........#.#........X.#........#.#........#.#........#.#........#
+#.##...#...#.##...#...#.X#...#...#.##...#...#.##...#...#.##...#...
+#...##....##...##....##...#X....##...##....##...##....##...##....#
+.#..#...#.#.#..#...#.#.#..#...X.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+
+In this example, traversing the map using this slope would cause you to encounter 7 trees.
+
+Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you encounter?
+
+ */
+
+package day3
+
+import A
+import java.lang.IllegalArgumentException
+
+enum class TerrainType(val char: Char) {
+    OPEN('.'),
+    TREE('#');
+
+    companion object {
+        fun forChar(c: Char) = values().find { c == it.char }
+            ?: throw IllegalArgumentException("unexpected terrain type: $c")
+    }
+}
+
+fun String.terrainTypes() = this.toCharArray().map { TerrainType.forChar(it) }.toTypedArray()
+
+fun main() {
+    val input = A::class.java.classLoader.getResource("day3/input")
+        .readText()
+        .lines()
+        .filter { it.isNotEmpty() }
+        .onEach { println(it) }
+        .also { println("total lines: ${it.size}") }
+
+    val terrain = mutableListOf<Array<TerrainType>>()
+        .apply { input.forEach { add(it.terrainTypes()) } }
+        .toTypedArray()
+        .also { println("total lines of terrain: ${it.size}") }
+
+    var x = 0
+    var encounteredTrees = 0
+    terrain.forEachIndexed { y, arrayOfTerrainTypes ->
+        if (y > 0) {
+            x = (x + 3) % arrayOfTerrainTypes.size
+            if (arrayOfTerrainTypes[x] == TerrainType.TREE) {
+                ++encounteredTrees
+            }
+            println("tree on $x,$y: ${arrayOfTerrainTypes[x] == TerrainType.TREE}")
+        }
+    }
+    println("solution: $encounteredTrees")
+}
